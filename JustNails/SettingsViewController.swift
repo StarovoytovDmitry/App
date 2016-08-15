d0d2a918f0196604e403fdc9682e52ad6a811035
Дмитрий <delfin@Air-Dmitrij.Dlink>
2016-08-15 23:18:23 +0300

    2

---
+++ JustNails/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  JustNails
+//
+//  Created by Дмитрий on 15.08.16.
+//  Copyright © 2016 Dmitry. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}
